b34140355e67b3b72c170786086cc631dc1c015b
87606 <thomvandervorst@gmail.com>
2025-09-03 10:17:41 +0200

    Back-end start game reset ui

--- js/gamescript.js
+++ js/gamescript.js
@@ -1,4 +1,4 @@
-const apiBase = 'https://www.deckofcardsapi.com/api/deck';
+const apiCards = 'https://www.deckofcardsapi.com/api/deck';
     let deckId = '';
     let currentCard = null;
     let nextCard = null;
@@ -23,6 +23,53 @@ const apiBase = 'https://www.deckofcardsapi.com/api/deck';
         
     }
 
-    async function startgame(params) {
+    function reset(){
+        round = 0;
+        document.getElementById('start-phase').style.display = 'block';
+        document.getElementById('round-1-phase').style.display ='none'
+        document.getElementById('round-2-phase').style.display = 'none'
+        document.getElementById('round-3-phase').style.display = 'none'
+        document.getElementById('round-4-phase').style.display = 'none'
         
+
     }
+
+    async function startgame() {
+        reset();
+        fetchJSON();
+        // get a new deck shuffled
+        const data = await fetchJSON(`${apiCards}/new/shuffledeck/?deck_count1}`)
+        deckId = data.deck_id;
+         drawOne('currentCard').then(() => {
+        document.getElementById('startGame').style.display = 'none';
+        document.getElementById('round-1-phase').style.display = 'block'
+      });
+    }
+
+    async function drawOne(target) {
+        const data = await fetchJSON(`${apiBase}/${deckId}/draw/?count=1`);
+        if (data.cards.length === 0) {
+          document.getElementById('message').textContent = 'No more cards. Game over!';
+          return;
+        }
+        const card = data.cards[0];
+        document.getElementById(target).src = card.image;
+        if (target === 'currentCard') currentCard = card;
+        else nextCard = card;
+      }
+
+      function evaluateGuess(guessType){
+        const valueCurrentCard = valueMap[currentCard.value];
+        const valueNextCard = valueMap[nextCard.value];
+
+        let correct = false;
+        //check conditions
+        if (guessType === 'higher') correct = valueNextCard>valueCurrentCard
+        else if (guessType === 'lower') correct = valueNextCard<valueCurrentCard
+        else if(guessType === 'same') correct = valueNextCard == valueCurrentCard
+      }
+
+
+   
+        
+ 
